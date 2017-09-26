82e1b6e9d6c19f3c53a5a9051060ef031ac13ec1
Chandler <chandlerazling@gmail.com>
2017-09-26 11:50:24 -0700

    quickbites case study

--- scripts/quickbiteschart.js
+++ scripts/quickbiteschart.js
@@ -2,6 +2,7 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart1);
 google.charts.setOnLoadCallback(drawChart2);
+var options = {tooltip: {isHtml: true, text: 'percentage'}, 'fontName': "Muli", width:800 , height: 300, 'fontSize': 14, 'fontWeight': 200, colors: [ '#FF4519','#FF6F00', '#fc950f', '#ffbb00', '#ffd449']};
 
 // Draw the chart and set the chart values
 function drawChart1() {
@@ -11,10 +12,8 @@ function drawChart1() {
   ['Figuring out what to order', 2],
   ['Ordering for others', 2],
   ['Wait time', 2],
-  ['Communicating with server', 1]
+  ['Communicating with server', 1] 
 ]);
-
-var options = {'fontName': "Muli", 'fontSize': 14, 'fontWeight': 200};
 
 var chart1 = new google.visualization.PieChart(document.getElementById('piechart1'));
   chart1.draw(data1, options);
@@ -23,12 +22,11 @@ var chart1 = new google.visualization.PieChart(document.getElementById('piechart
 function drawChart2() {
   var data2 = google.visualization.arrayToDataTable([
   ['Task', 'Hours per Day'],
-  ['Better menus',2],
   ['Easier communication', 4],
+  ['Better menus',2],
   ['Mobile payment', 1],
   ['Less pressure to order quickly', 1],
 ]);
-var options = {'fontName': "Muli", 'fontSize': 14, 'fontWeight': 200};
 
 var chart2 = new google.visualization.PieChart(document.getElementById('piechart2'));
   chart2.draw(data2, options);
